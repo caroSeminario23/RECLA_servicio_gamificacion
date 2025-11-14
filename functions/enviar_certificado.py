32feec40dbca368ca89494294b307c692cc47635
caroSeminario23 <carolina.seminario@unmsm.edu.pe>
2025-11-13 20:16:46 -0500

    feat(enviar_certificado): actualizar función para enviar certificado en formato HTML

--- functions/enviar_certificado.py
+++ functions/enviar_certificado.py
@@ -1,13 +1,25 @@
 from flask_mail import Message
 from utils.mail import mail
 
-def enviar_certificado_por_correo(username, destinatario, pdf_url):
+def enviar_certificado_por_correo(username, destinatario, certificado_url):
     try:
         asunto = "RECLA - Certificado emitido"
-        cuerpo = f"Felicidades {username}, tu certificado ha sido emitido. Puedes descargarlo aquí: {pdf_url}"
+        #cuerpo = f"Felicidades {username}, tu certificado ha sido emitido. Puedes descargarlo aquí: {certificado_url}"
         
+        cuerpo_html = f"""
+        <html>
+            <body>
+                <p>Felicidades <strong>{username}</strong>, tu certificado ha sido emitido.</p>
+                <br>
+                <img src="{certificado_url}" alt="Certificado" style="max-width: 600px; height: auto;">
+                <br>
+                <p>Recuerda que puedes descargarlo desde haciendo click <a href="{certificado_url}">aquí</a>.</p>
+            </body>
+        </html>
+        """
+
         msg = Message(subject=asunto, recipients=[destinatario])
-        msg.body = cuerpo
+        msg.html = cuerpo_html
         mail.send(msg)
         print("Certificado enviado exitosamente.")
         return True

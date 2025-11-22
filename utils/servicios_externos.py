ea60f326116b56d3c8cc9ab20764835c91686527
caroSeminario23 <carolina.seminario@unmsm.edu.pe>
2025-11-22 10:56:54 -0500

    feat(api): actualizar URL_BASE a la versión en producción y agregar ejemplos de uso para la presentación y desbloqueo de stickers

--- utils/servicios_externos.py
+++ utils/servicios_externos.py
@@ -1,4 +1,4 @@
-URL_BASE = "http://localhost:5000"
+URL_BASE = "https://recla-servicio-usuarios.onrender.com"; # "http://localhost:5000"
 
 VERIFICADOR_PUNTOS_INSIGNIA = f"{URL_BASE}/estatus_routes/verificar_puntos_insignia"
 
